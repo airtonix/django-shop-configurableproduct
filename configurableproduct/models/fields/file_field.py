32c920a8c3b1950836b77e8c98e2c7fd507b28b8
Zenobius Jiricek <airtonix@gmail.com>
2013-08-21 08:55:16 +0930

    reference models for FileField

--- configurableproduct/models/fields/file_field.py
+++ configurableproduct/models/fields/file_field.py
@@ -4,7 +4,7 @@
 __author__ = 'zeus'
 __contributers__ = [
     'Zenobius Jiricek <airtonix@gmail.com>',
-    ]
+]
 
 import os
 import uuid
@@ -13,22 +13,17 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from .abstract import (
-    ProductAbstractField,
-    BasicThroughField,
-    ProductAbstractFieldThrough,
-    TypeAbstractFieldTrhough
-    )
+from . import abstract
 
 
-#noinspection PyUnusedLocal
+# noinspection PyUnusedLocal
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
     return os.path.join(getattr(settings, 'PRODUCT_FILE_UPLOAD_TO', os.path.join('products', 'files')), filename)
 
 
-class ProductFileField(ProductAbstractField):
+class ProductFileField(abstract.ProductAbstractField):
 
     class Meta(object):
         verbose_name = _('Product field - file')
@@ -36,21 +31,24 @@ class ProductFileField(ProductAbstractField):
         app_label = 'configurableproduct'
 
 
-class ProductFile(ProductAbstractFieldThrough):
-    class Meta(ProductAbstractFieldThrough.Meta):
+class ProductFile(abstract.ProductAbstractFieldThrough):
+
+    class Meta(abstract.ProductAbstractFieldThrough.Meta):
         verbose_name = _('Product file field')
         verbose_name_plural = _('Product file field')
         app_label = 'configurableproduct'
 
-    description = models.CharField(max_length=200, verbose_name=_('Description'), blank=True, null=True)
-    value = FileField(verbose_name=_('File'), upload_to=get_file_path)
-    field = models.ForeignKey(ProductFileField, verbose_name=_('Field'))
+    description = models.CharField(
+        max_length=200, verbose_name=_('Description'), blank=True, null=True)
+    value = models.FileField(verbose_name=_('File'), upload_to=get_file_path)
+    field = models.ForeignKey('ProductFileField', verbose_name=_('Field'))
 
 
-class TypeFile(TypeAbstractFieldTrhough):
-    class Meta(TypeAbstractFieldTrhough.Meta):
+class TypeFile(abstract.TypeAbstractFieldThrough):
+
+    class Meta(abstract.TypeAbstractFieldThrough.Meta):
         verbose_name = _('File field')
         verbose_name_plural = _('File fields')
         app_label = 'configurableproduct'
 
-    field = models.ForeignKey(ProductFileField, verbose_name=_('Field'))
+    field = models.ForeignKey('ProductFileField', verbose_name=_('Field'))
