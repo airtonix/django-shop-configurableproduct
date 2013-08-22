c207d27ca17ae02b9b8775515382cb05dbc7cb45
Zenobius Jiricek <airtonix@gmail.com>
2013-08-22 16:28:30 +0930

    inherit from configurableproductbase and baseproduct

--- configurableproduct/models/defaults/product/default.py
+++ configurableproduct/models/defaults/product/default.py
@@ -8,10 +8,18 @@ __contributers__ = [
 
 from django.utils.translation import ugettext_lazy as _
 
+from shop.models_bases import BaseProduct
+from shop.models_bases.managers import (
+    ProductManager,
+    ProductStatisticsManager,
+)
+
 from . import base
 
 
-class Product(base.ConfigurableProductBase):
+class Product(base.ConfigurableProductBase, BaseProduct):
+    objects = ProductManager()
+    statistics = ProductStatisticsManager()
 
     class Meta:
         verbose_name = _('Product')
